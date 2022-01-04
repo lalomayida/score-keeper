63b033303493f7764e7f663001fe295eb0df8108
Luis Eduardo Mayida González <51863317+lalomayida@users.noreply.github.com>
2022-01-03 20:28:32 -0600

    Implementing selector

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const p1Button = document.querySelector("#p1Button");
 const p2Button = document.querySelector("#p2Button");
+const resetButton = document.querySelector("#resetButton");
 const p1Display = document.querySelector("#p1Display");
 const p2Display = document.querySelector("#p2Display");
+const winningScoreSelect = document.querySelector("#playTo");
 
 let p1Score = 0;
 let p2Score = 0;
@@ -20,12 +22,28 @@ p1Button.addEventListener("click", function () {
 });
 
 p2Button.addEventListener("click", function () {
-    if (!isGameOver) {
-      p2Score += 1;
-  
-      if (p2Score === winningScore) {
-        isGameOver = true;
-      }
-      p2Display.textContent = p2Score;
+  if (!isGameOver) {
+    p2Score += 1;
+
+    if (p2Score === winningScore) {
+      isGameOver = true;
     }
-  });
+    p2Display.textContent = p2Score;
+  }
+});
+
+resetButton.addEventListener("click", reset());
+
+winningScoreSelect.addEventListener("change", function () {
+  isGameOver = false;
+  winningScore = parseInt(this.value);
+reset();
+});
+
+function reset(){
+    isGameOver = false;
+    p1Score = 0;
+    p2Score = 0;
+    p1Display.textContent = 0;
+    p2Display.textContent = 0;
+}
